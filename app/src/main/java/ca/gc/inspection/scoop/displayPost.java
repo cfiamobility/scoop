2f48c6ec69f07145ead4300b2f970ba77d317223
wongnic <Nicole.Wong@canada.ca>
2019-04-02 15:34:15 -0400

    updated rotate image xml

---
+++ app/src/main/java/ca/gc/inspection/scoop/displayPost.java
@@ -0,0 +1,13 @@
+package ca.gc.inspection.scoop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class displayPost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_post);
+    }
+}
