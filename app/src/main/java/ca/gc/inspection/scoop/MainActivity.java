68b4c793d84000365040211f7d2aa416c7ca11a9
Timmy Truong <timmy.truong@canada.ca>
2019-04-03 09:52:46 -0400

    test

--- app/src/main/java/ca/gc/inspection/scoop/MainActivity.java
+++ app/src/main/java/ca/gc/inspection/scoop/MainActivity.java
@@ -3,6 +3,7 @@ package ca.gc.inspection.scoop;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, edit_profile_screen.class);
+        startActivity(intent);
 
         Log.i("TEST", "Hello World");
         //deleted the commend to test jenkins
