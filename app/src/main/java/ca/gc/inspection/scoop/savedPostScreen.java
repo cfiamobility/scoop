311ee66b5cde64a95b493e94c71ff86e1281a7f8
wongnic <Nicole.Wong@canada.ca>
2019-04-04 08:37:28 -0400

    updated tab layouts

--- app/src/main/java/ca/gc/inspection/scoop/savedPostScreen.java
+++ app/src/main/java/ca/gc/inspection/scoop/savedPostScreen.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -47,5 +48,6 @@ public class savedPostScreen extends AppCompatActivity {
         // setting up the custom adapter for the recycler view
         adapter = new feedAdapter(test);
         recyclerView.setAdapter(adapter);
+
     }
 }
