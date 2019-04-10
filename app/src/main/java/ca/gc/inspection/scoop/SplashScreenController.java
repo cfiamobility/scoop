aac3158335613f5c48b54efa5d9f2e93bba23f54
YangMA <maxwell.yang@canada.ca>
2019-04-10 13:24:03 -0400

    changed splash screen controller

--- app/src/main/java/ca/gc/inspection/scoop/SplashScreenController.java
+++ app/src/main/java/ca/gc/inspection/scoop/SplashScreenController.java
@@ -10,7 +10,9 @@ public class SplashScreenController {
     public static void goToMainScreen(Context context, Activity activity){
         SharedPreferences sharedPreferences = context.getSharedPreferences("ca.gc.inspection.scoop", Context.MODE_PRIVATE);
         if(!sharedPreferences.getString("userId", "nothing").equals("nothing")){
+            String userId = sharedPreferences.getString("userId", "nothing");
             Intent intent = new Intent(context, mainScreen.class);
+            Config.currentUser = userId;
             context.startActivity(intent);
             activity.finish();
         }
