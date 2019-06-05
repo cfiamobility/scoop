3adfa8ea1c7189154d170f9ba1e5b48ab568016d
Kenneth Zheng <kenneth.zheng@canada.ca>
2019-06-05 10:18:37 -0400

    AMD-180: added EditProfilePresenter

---
+++ app/src/main/java/ca/gc/inspection/scoop/EditProfileContract.java
@@ -0,0 +1,14 @@
+package ca.gc.inspection.scoop;
+
+import ca.gc.inspection.scoop.base.BasePresenter;
+import ca.gc.inspection.scoop.base.BaseView;
+
+public interface EditProfileContract {
+    interface View extends BaseView<Presenter> {
+
+    }
+
+    interface Presenter extends BasePresenter {
+
+    }
+}
