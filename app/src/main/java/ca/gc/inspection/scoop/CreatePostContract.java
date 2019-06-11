23679eda1fb75ec78768da5c70222a0a5f4d5c4f
Kenneth Zheng <kenneth.zheng@canada.ca>
2019-06-11 09:02:56 -0400

    AMD-123: refactored CreatePostPresenter and Interactor

--- app/src/main/java/ca/gc/inspection/scoop/CreatePostContract.java
+++ app/src/main/java/ca/gc/inspection/scoop/CreatePostContract.java
@@ -14,23 +14,6 @@ public interface CreatePostContract {
 
     interface Presenter extends BasePresenter {
 
-        static Map<String, String> getPostParams(String userId, String title, String text, String imageBitmap) {
-            Map<String, String>  params = new HashMap<>();
-            params.put("userid", userId); // post test user
-            params.put("activitytype", Integer.toString(Config.postType));
-            params.put("posttitle", title);
-            params.put("posttext", text);
-            params.put("postimage", imageBitmap);
-            return params;
-        }
-
-        static Map<String,String> getPostHeaders() {
-            // inserting the token into the response header that will be sent to the server
-            Map<String, String> header = new HashMap<>();
-            header.put("authorization", Config.token);
-            return header;
-        }
-
         void sendPostToDatabase(MySingleton singleton, final String userId, final String title, final String text, final String imageBitmap);
     }
 }
