8797e6f1be60212b5e74c3265e375fbeb9c55031
Kenneth Zheng <kenneth.zheng@canada.ca>
2019-06-12 10:38:42 -0400

    AMD-135: refactored InfoActivity into MVP architecture

---
+++ app/src/main/java/ca/gc/inspection/scoop/InfoContract.java
@@ -0,0 +1,14 @@
+package ca.gc.inspection.scoop;
+
+import ca.gc.inspection.scoop.base.BasePresenter;
+import ca.gc.inspection.scoop.base.BaseView;
+
+public interface InfoContract {
+    interface View extends BaseView<Presenter> {
+
+    }
+
+    interface Presenter extends BasePresenter {
+
+    }
+}
