577491666ec8d88106a06ef351b2de4b5e1aab47
chanjosh <josh.chan4@canada.ca>
2019-08-30 14:55:36 -0400

    merged with master

--- app/src/main/java/ca/gc/inspection/scoop/createpost/CreatePostActivity.java
+++ app/src/main/java/ca/gc/inspection/scoop/createpost/CreatePostActivity.java
@@ -1,6 +1,5 @@
 package ca.gc.inspection.scoop.createpost;
 
-import ca.gc.inspection.scoop.R;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -9,6 +8,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -16,7 +16,6 @@ import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputFilter;
 import android.text.TextWatcher;
@@ -35,6 +34,7 @@ import java.util.Locale;
 
 import ca.gc.inspection.scoop.Config;
 import ca.gc.inspection.scoop.ImageFilePath;
+import ca.gc.inspection.scoop.R;
 import ca.gc.inspection.scoop.util.CameraUtils;
 import ca.gc.inspection.scoop.util.NetworkUtils;
 import de.hdodenhof.circleimageview.CircleImageView;
